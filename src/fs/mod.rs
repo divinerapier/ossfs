297d51d4e61c9b0b981d4756809e41accb666a4e
divinerapier <poriter.coco@gmail.com>
2019-09-19 18:51:42 +0800

    feature: simple testing
    Signed-off-by: divinerapier <poriter.coco@gmail.com>
    
    Signed-off-by: divinerapier <poriter.coco@gmail.com>

---
+++ src/fs/mod.rs
@@ -0,0 +1,2 @@
+pub mod cachefs;
+pub mod inode;
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod fs;
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_api_parameters() {
+        env_logger::from_env(env_logger::Env::default().default_filter_or("trace")).init();
+        log::set_max_level(log::LevelFilter::max());
+
+        let fs = super::fs::cachefs::CacheFs {};
+        let mountpoint = "./mnt";
+        let options = ["-o", "ro", "-o", "fsname=cachefs"]
+            .iter()
+            .map(|o| o.as_ref())
+            .collect::<Vec<&std::ffi::OsStr>>();
+        fuse::mount(fs, &mountpoint, &options).unwrap();
+    }
+}
