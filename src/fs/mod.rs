9ef896eee04be89f9c4c409229b290aa73467488
divinerapier <poriter.coco@gmail.com>
2019-09-27 18:33:24 +0800

    use s3
    Signed-off-by: divinerapier <poriter.coco@gmail.com>
    
    Signed-off-by: divinerapier <poriter.coco@gmail.com>

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -2,6 +2,7 @@ pub mod backend;
 pub mod filesystem;
 pub mod fuse;
 pub mod node;
+pub mod s3;
 pub mod stat;
 
 pub use self::backend::SimpleBackend;
---
+++ src/fs/s3.rs
@@ -0,0 +1,48 @@
+use super::backend::Backend;
+use super::node::Node;
+use super::stat::Stat;
+use fuse::FileType;
+use rusoto_core::credential::ChainProvider;
+use rusoto_core::request::HttpClient;
+use rusoto_core::Region;
+use rusoto_s3::{S3Client, S3};
+use std::fmt::Debug;
+use std::path::Path;
+use std::time::{Duration, Instant};
+
+pub struct S3Backend {
+    client: S3Client,
+}
+
+impl S3Backend {
+    pub fn new() -> S3Backend {
+        let mut chain = ChainProvider::new();
+        chain.set_timeout(Duration::from_millis(200));
+        let s3client = S3Client::new_with(
+            HttpClient::new().expect("failed to create request dispatcher"),
+            chain,
+            Region::UsEast1,
+        );
+        unimplemented!()
+    }
+}
+
+impl Backend for S3Backend {
+    fn root(&self) -> Node {
+        unimplemented!()
+    }
+    fn get_children<P: AsRef<Path> + Debug>(&self, path: P) -> Result<Vec<Node>, String> {
+        Err(String::from(""))
+    }
+    fn statfs<P: AsRef<Path> + Debug>(&self, path: P) -> Option<Stat> {
+        None
+    }
+    fn mknod<P: AsRef<Path> + Debug>(
+        &self,
+        path: P,
+        filetype: FileType,
+        mode: u32,
+    ) -> Result<(), std::io::Error> {
+        unimplemented!()
+    }
+}
