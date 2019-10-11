10636a6585cff7f56915b91024d261de5a10233f
divinerapier <poriter.coco@gmail.com>
2019-10-11 14:57:37 +0800

    add cmdline interface

--- examples/simple-server/main.rs
+++ examples/simple-server/main.rs
@@ -1,6 +1,31 @@
+use clap::{App, Arg};
 use std::env;
-
 fn main() {
+    let matches = App::new("simple-server")
+        .version("1.0")
+        .author("divinerapier")
+        .about("a simple ossfs server")
+        .arg(
+            Arg::with_name("backend")
+                .required(false)
+                .short("b")
+                .long("backend")
+                .value_name("BACKEND")
+                .help("Sets the custom backend")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("cache")
+                .required(false)
+                .short("c")
+                .long("cache")
+                .help("Sets the number of files")
+                .takes_value(false),
+        )
+        .get_matches();
+    let backend: String = matches.value_of("backend").unwrap().to_owned();
+    let enable_cache: bool = matches.is_present("cache");
+
     env_logger::from_env(
         env_logger::Env::default()
             .default_filter_or(env::var("LOG_LEVEL").unwrap_or(String::from("debug"))),
@@ -11,7 +36,7 @@ fn main() {
     let mountpoint = env::var("MOUNT_POINT").unwrap_or(String::from("./mnt"));
     let rootpath = env::var("ROOT_PATH").unwrap_or(String::from("./root"));
 
-    let fs = ossfs::Fuse::new(ossfs::SimpleBackend::new(rootpath), false);
+    let fs = ossfs::Fuse::new(ossfs::SimpleBackend::new(rootpath), enable_cache);
     // let fs = super::Fuse::new(super::S3Backend::new(
     //     "http://172.21.20.134:9001",
     //     "5577006791947779410",
